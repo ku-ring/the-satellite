0cddd673c72d3b412bd0492373f97b9abd03d12c
Jaesung Lee <jaesung.lee@sendbird.com>
2023-04-15 02:35:33 +0900

    [MOD] Updated build_and_test.yml to enable testing

--- Tests/SatelliteTests/SatelliteTests.swift
+++ Tests/SatelliteTests/SatelliteTests.swift
@@ -11,13 +11,13 @@ final class SatelliteTests: XCTestCase {
             }
         }
     }
-
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssert(true)
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func test_requestURL() throws {
